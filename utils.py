8ce168f1b4ac984094b3f2bc2523db8bad27c09f
Zhang Yunjun <yunjunz@users.noreply.github.com>
2019-04-17 23:54:03 -0400

    utils: add utils functions for isce/roipac files
    + bug fix for glob2rdr() when input latl/lonl is 2D
    
    + add read_roipac_data() to read common ROIPAC binary file into 2D array
    
    + add read_roipac_rsc() to read .rsc file into dict
    
    + add get_isce_lalo_ref() to grab LAT/LON_REF1/2/3/4 info from ISCE lat/lon.rdr file
    
    + add read_isce_data() to read ISCE binary file into 2D array
    
    + add read_isce_xml() to read ISCE xml file into dict
    
    + read_metadata() for both isce and roipac, as an generic version for rd_rsc()
    
    + add read_data() to call read_isce_data() and read_roipac_data()
    
    + add get_lat_lon() to get 2D lat/lon from rsc file

--- utils.py
+++ utils.py
@@ -6,10 +6,249 @@
 # Ecole Normale Superieure, Paris                          #
 # Contact: insar@geologie.ens.fr                           #
 ############################################################
+
+
+import os
+import sys
+import time
 import numpy as np
 import matplotlib.pyplot as plt
-import time
-import sys
+import xml.etree.ElementTree as ET
+
+
+###############Read ISCE / ROIPAC file###############
+def get_lat_lon(rscfile):
+    """Get 2D lat and lon from rsc file"""
+    meta = read_roipac_rsc(rscfile)
+    length, width = int(meta['FILE_LENGTH']), int(meta['WIDTH'])
+
+    # get bbox
+    if 'Y_FIRST' in meta.keys():
+        # geo coordinates
+        lat0 = float(meta['Y_FIRST'])
+        lon0 = float(meta['X_FIRST'])
+        lat_step = float(meta['Y_STEP'])
+        lon_step = float(meta['X_STEP'])
+        lat1 = lat0 + lat_step * (length - 1)
+        lon1 = lon0 + lon_step * (width - 1)
+    else:
+        # radar coordinates
+        lats = [float(meta['LAT_REF{}'.format(i)]) for i in [1,2,3,4]]
+        lons = [float(meta['LON_REF{}'.format(i)]) for i in [1,2,3,4]]
+        lat0 = np.mean(lats[0:2])
+        lat1 = np.mean(lats[2:4])
+        lon0 = np.mean(lons[0:3:2])
+        lon1 = np.mean(lons[1:4:2])
+
+    # bbox --> 2D array
+    lat, lon = np.mgrid[lat0:lat1:length*1j,
+                        lon0:lon1:width*1j]
+    return lat, lon
+
+
+def read_data(fname, dname='inc'):
+    """Read 2D data from ISCE / ROIPAC file"""
+    # get meta file extension
+    meta_exts = [i for i in ['.xml', '.rsc'] if os.path.isfile(fname+i)]
+    if len(meta_exts) == 0:
+        raise FileNotFoundError('No metadata file found for data file: {}'.format(fname))
+    meta_ext = meta_exts[0]
+
+    if meta_ext == '.xml':
+        data = read_isce_data(fname, dname=dname)
+    elif meta_ext == '.rsc':
+        data = read_roipac_data(fname)
+    return data
+
+
+def read_metadata(fname, latfile=None, lonfile=None, full=False, verbose=False):
+    '''Reading metadata for ISCE or ROIPAC style data file
+
+    Args:
+        * fname (str): Path to the ROIPAC or ISCE data file.
+
+    Returns:
+        * lat (np.array) : Array of lat of the 4 corners.
+        * lon (np.array) : Array of lon of the 4 corners.
+        * nx  (np.int)   : Number of range bins.
+        * ny  (np.int)   : Number of azimuth lines.
+        * dpix (np.float): Average pixel spacing.
+        * meta (dict)    : Dictionary of metadata [return if full==True]
+    '''
+    # get meta file extension
+    meta_exts = [i for i in ['.rsc','.xml'] if os.path.isfile(fname+i)]
+    if len(meta_exts) == 0:
+        raise FileNotFoundError('No metadata file found for data file: {}'.format(fname))
+    meta_ext = meta_exts[0]
+
+    if verbose:
+        print("PROGRESS: READING {} FILE".format(fname+meta_ext))
+
+    # read metadata
+    meta = {}
+    if meta_ext == '.rsc':
+        meta = read_roipac_rsc(fname+meta_ext)
+
+    elif meta_ext == '.xml':
+        meta = read_isce_xml(fname+meta_ext)
+
+        # default latfile
+        if not latfile:
+            latfile = os.path.join(os.path.dirname(xmlfile),'lat.rdr')
+            if not os.path.isfile(latfile):
+                raise FileNotFoundError("No latitude file found in ISCE style!")
+
+        # default lonfile
+        if not lonfile:
+            lonfile = os.path.join(os.path.dirname(xmlfile),'lon.rdr')
+            if not os.path.isfile(lonfile):
+                raise FileNotFoundError("No longitude file found in ISCE style!")
+
+        meta.update(get_isce_lalo_ref(latfile, lonfile))
+
+    # prepare output
+    nx = int(meta['WIDTH'])
+    ny = int(meta['FILE_LENGTH'])
+    lat = np.zeros((4,1))
+    lon = np.zeros((4,1))
+    lat[0] = float(meta['LAT_REF1'])
+    lon[0] = float(meta['LON_REF1'])
+    lat[1] = float(meta['LAT_REF2'])
+    lon[1] = float(meta['LON_REF2'])
+    lat[2] = float(meta['LAT_REF3'])
+    lon[2] = float(meta['LON_REF3'])
+    lat[3] = float(meta['LAT_REF4'])
+    lon[3] = float(meta['LON_REF4'])
+    rgpix = 90.0  #following rd_rsc()
+    azpix = 90.0  #following rd_rsc()
+    dpix = np.sqrt(6.25*rgpix*rgpix + azpix*azpix)
+    if full:
+        return lon,lat,nx,ny,dpix,meta
+    else:
+        return lon,lat,nx,ny,dpix
+
+
+def read_isce_xml(xmlfile):
+    """Read ISCE XML file into dict"""
+    meta = {}
+    root = ET.parse(xmlfile).getroot()
+    if root.tag.startswith('image'):
+        for child in root.findall('property'):
+            key = child.get('name')
+            value = child.find('value').text
+            meta[key] = value
+
+    # convert key name from isce to roipac
+    isce2roipacKeyDict = {
+        'width':'WIDTH',
+        'length':'FILE_LENGTH',
+    }
+    for key,value in isce2roipacKeyDict.items():
+        meta[value] = meta[key]
+    return meta
+
+
+def read_isce_data(fname, dname=None):
+    """Read ISCE data file"""
+    # read xml file
+    xml_file = fname+'.xml'
+    meta = read_isce_xml(xml_file)
+
+    # get data_type
+    dataTypeDict = {
+        'byte': 'bool_',
+        'float': 'float32',
+        'double': 'float64',
+        'cfloat': 'complex64',
+    }
+    data_type = dataTypeDict[meta['data_type'].lower()]
+
+    width = int(meta['width'])
+    length = int(meta['length'])
+    num_band = int(meta['number_bands'])
+    band = 1
+    if fname.startswith('los') and dname and dname.startswith('az'):
+        band = 2
+
+    # read
+    data = np.fromfile(fname, dtype=data_type, count=length*width*num_band).reshape(-1, width*num_band)
+    data = data[:, width*(band-1):width*band]
+    return data
+
+
+def get_isce_lalo_ref(lat_file, lon_file):
+    """Get LAT/LON_REF1/2/3/4 value from ISCE lat/lon.rdr file"""
+
+    def get_nonzero_row_number(data, buffer=2):
+        """Find the first and last row number of rows without zero value
+        for multiple swaths data
+        """
+        if np.all(data):
+            r0, r1 = 0 + buffer, -1 - buffer
+        else:
+            row_flag = np.sum(data != 0., axis=1) == data.shape[1]
+            row_idx = np.where(row_flag)[0]
+            r0, r1 = row_idx[0] + buffer, row_idx[-1] - buffer
+        return r0, r1
+
+    meta = {}
+    # read LAT/LON_REF1/2/3/4 from lat/lonfile
+    lat = read_isce_data(lat_file)
+    r0, r1 = get_nonzero_row_number(lat)
+    meta['LAT_REF1'] = str(lat[r0, 0])
+    meta['LAT_REF2'] = str(lat[r0, -1])
+    meta['LAT_REF3'] = str(lat[r1, 0])
+    meta['LAT_REF4'] = str(lat[r1, -1])
+
+    lon = read_isce_data(lon_file)
+    r0, r1 = get_nonzero_row_number(lon)
+    meta['LON_REF1'] = str(lon[r0, 0])
+    meta['LON_REF2'] = str(lon[r0, -1])
+    meta['LON_REF3'] = str(lon[r1, 0])
+    meta['LON_REF4'] = str(lon[r1, -1])
+    return meta
+
+
+def read_roipac_rsc(rscfile):
+    """Read ROIPAC style RSC file into dict"""
+    meta = {}
+    f = open(rscfile,'r')
+    line = f.readline()
+    while line:
+        llist = line.split()
+        if len(llist)>0 :
+            meta[llist[0]] = llist[1]
+        line = f.readline()
+    f.close()
+    return meta
+
+
+def read_roipac_data(fname):
+    """Read ROIPAC data file"""
+    # get length / width
+    rsc_file = fname+'.rsc'
+    meta = read_roipac_rsc(rsc_file)
+    length = int(meta['FILE_LENGTH'])
+    width = int(meta['WIDTH'])
+
+    # get datatype / band
+    data_type = 'float32'
+    num_band = 1
+    band = 1
+    fext = os.path.splitext(fname)[1]
+    if fext == '.dem':
+        data_type = 'int16'
+    elif fext == '.hgt':
+        num_band = 2
+        band = 2
+
+    # read
+    data = np.fromfile(fname, dtype=data_type, count=length*width*num_band).reshape(-1, width*num_band)
+    data = data[:, width*(band-1):width*band]
+    return data
+###############Read ISCE / ROIPAC file - end###############
+
+
 ###############Reading input RSC file for radar###############
 def rd_rsc(inname,full=False,verbose=False):
     '''Reading a ROI-PAC style RSC file.
@@ -42,12 +281,13 @@ def rd_rsc(inname,full=False,verbose=False):
     rpacdict['LAT_REF4']=0.0
     rpacdict['LON_REF4']=0.0
     while line:
-       llist = line.split()
-       if len(llist)>0 :
-          rpacdict[llist[0]] = llist[1]
-       line = infile.readline()
+        llist = line.split()
+        if len(llist)>0 :
+            rpacdict[llist[0]] = llist[1]
+        line = infile.readline()
     infile.close()
 
+    # prepare output
     nx = np.int(rpacdict['WIDTH'])
     ny = np.int(rpacdict['FILE_LENGTH'])
     lat=np.zeros((4,1))
@@ -60,8 +300,8 @@ def rd_rsc(inname,full=False,verbose=False):
     lon[2] = np.float(rpacdict['LON_REF3'])
     lat[3] = np.float(rpacdict['LAT_REF4'])
     lon[3] = np.float(rpacdict['LON_REF4'])
-#    rgpix = np.float(rpacdict['RANGE_PIXEL_SIZE'])
-#    azpix = np.float(rpacdict['AZIMUTH_PIXEL_SIZE'])
+    #rgpix = np.float(rpacdict['RANGE_PIXEL_SIZE'])
+    #azpix = np.float(rpacdict['AZIMUTH_PIXEL_SIZE'])
     rgpix = 90.0
     azpix = 90.0
     dpix = np.sqrt(6.25*rgpix*rgpix+azpix*azpix)
@@ -69,9 +309,8 @@ def rd_rsc(inname,full=False,verbose=False):
         return lon,lat,nx,ny,dpix,rpacdict
     else:
         return lon,lat,nx,ny,dpix
-
-
 #######################Finished rd_rsc###########################
+
 
 ###############Reading input RSC file for geo###############
 def geo_rsc(inname,full=False,verbose=False):
@@ -113,7 +352,7 @@ def geo_rsc(inname,full=False,verbose=False):
 
     dx = np.float(rpacdict['X_STEP'])
     dy = np.float(rpacdict['Y_STEP'])
-    
+
     lat[0] = lat[1] + dy*ny
     lon[1] = lon[0] + dx*nx
     
@@ -134,7 +373,7 @@ def lonlat2rdr(lon, lat, lonlist, latlist, plotflag=False):
     Args:
         * lon               : Longitude array of the radar scene size=(ny,nx)
         * lat               : Latitude array of the radar scene size=(ny,nx)
-        * lonlist           : Longitude list of weeather stations
+        * lonlist           : Longitude list of the weeather stations
         * latlist           : Latitude list of the weather stations.
 
     Kwargs:
@@ -223,15 +462,15 @@ def glob2rdr(nx,ny,lat,lon,latl,lonl,plotflag='n'):
         * Fourth point is (nx,ny) i.e. Far Range, Last Lane <==> Lat[4],Lon[4] '''
 
     # Mapping function
-    A=np.hstack([lat,lon,np.ones((4,1))])
-    b=np.array([[1, 1],[nx, 1],[1, ny],[nx,ny]])
-    mfcn=np.linalg.lstsq(A,b)[0]
+    A = np.hstack([lat,lon,np.ones((4,1))])
+    b = np.array([[1, 1],[nx, 1],[1, ny],[nx,ny]])
+    mfcn = np.linalg.lstsq(A,b,rcond=None)[0]
 
     #Get grid points xi yi coordinates from this mapping function
-    nstn=len(latl)
-    A=np.array([latl, lonl, np.ones(nstn)]).T
-    xi=np.dot(A,mfcn[:,0])
-    yi=np.dot(A,mfcn[:,1])
+    nstn = latl.size
+    A = np.array([latl.reshape(-1,1), lonl.reshape(-1,1), np.ones((nstn,1))]).T
+    xi = np.dot(A,mfcn[:,0]).reshape(latl.shape)
+    yi = np.dot(A,mfcn[:,1]).reshape(latl.shape)
 
     if plotflag in ('y','Y'):
         plt.figure(1)
@@ -259,7 +498,9 @@ def glob2rdr(nx,ny,lat,lon,latl,lonl,plotflag='n'):
 
 ##########Conversion from Geo coordinate to Radar coordinates######
 def rdr2glob(wid,lgt,lat,lon,x,y,plotflag='n'):   #nx,ny,lat,lon,latl,lonl,plotflag='n'):
-    '''Transfert these radar geometry (x,y) coordinates in lat/lon coordinates with a simple linear transformation given the image width/length and the lat/lon coordinates of the 4 corners
+    '''Transfert these radar geometry (x,y) coordinates in lat/lon coordinates with a 
+    simple linear transformation given the image width/length and the lat/lon coordinates
+    of the 4 corners
 
     Args:
             * wid  (np.int)   : Width of the image (i.e. number of range bins)
@@ -285,16 +526,15 @@ def rdr2glob(wid,lgt,lat,lon,x,y,plotflag='n'):   #nx,ny,lat,lon,latl,lonl,plotf
             * Third  point is (1,ny)  i.e. Near Range, Last Lane <==> Lat[3],Lon[3]
             * Fourth point is (nx,ny) i.e. Far Range, Last Lane <==> Lat[4],Lon[4] '''
 
-    A=np.array([[1, 1, 1.],[wid, 1, 1.],[1, lgt, 1.],[wid, lgt, 1.]])
-    #b=np.array([[lat[0],lon[0]],[lat[1],lon[1]],[lat[2],lon[2]],[lat[3],lon[3]]])
+    A = np.array([[1, 1, 1.],[wid, 1, 1.],[1, lgt, 1.],[wid, lgt, 1.]])
     b = np.hstack((lat,lon))
-    mfcn=np.linalg.lstsq(A,b)[0]
+    mfcn = np.linalg.lstsq(A,b)[0]
 
     #Get grid points xi yi coordinates from this mapping function
-    nstn=len(x)
-    A=np.array([x, y, np.ones((nstn,1))]).T
-    lati=np.dot(A,mfcn[:,0])
-    loni=np.dot(A,mfcn[:,1])
+    nstn = len(x)
+    A = np.array([x, y, np.ones((nstn,1))]).T
+    lati = np.dot(A,mfcn[:,0])
+    loni = np.dot(A,mfcn[:,1])
 
     if plotflag in ('y','Y'):
         plt.figure(1)
@@ -380,7 +620,9 @@ class ProgressBar:
             percentString = ' ' + str(percentDone) + '% '
             elapsed_time = time.time() - self.start_time
             # slice the percentage into the bar
-            self.progBar = ''.join([self.progBar[0:percentPlace],percentString, self.progBar[percentPlace+len(percentString):], ])
+            self.progBar = ''.join([self.progBar[0:percentPlace],
+                                    percentString,
+                                    self.progBar[percentPlace+len(percentString):]])
             if percentDone > 0:
                 self.progBar += ' %6ds / %6ds' % (int(elapsed_time),int(elapsed_time*(100.//percentDone-1)))
 
