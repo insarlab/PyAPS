4a3528454deda35677bdbf2f4000ab0d18e1b22e
Zhang Yunjun <yunjunzgeo@gmail.com>
2021-11-15 12:49:47 -0800

    modernize tests dir
    + add test_calc.py and test_dload.py
    
    + tests/data dir: move ISCE geometry files to here
    
    + tests/data/ERA5: add two ERA5 dataset for testing of test_calc.py
    
    + tests/legacy: move unused TestMERRA.py and TestPyAPS.py
    
    + examples: remove obsolete 20061016.dat and TestECMWF.ipynb

---
+++ tests/test_dload.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# Author: Zhang Yunjun, Nov 15, 2021
+
+
+import os
+import pyaps3 as pa
+
+print('------------------------------------------------')
+print('import pyaps3 from {}'.format(pa.__file__))
+print('------------------------------------------------')
+print('test ERA5 data download')
+print('NOTE: Account setup is required on the Copernicus Climate Data Store (CDS).')
+print('      More detailed info can be found on: https://retostauffer.org/code/Download-ERA5/')
+print('      Add your account info to ~/.cdsapirc file.')
+pa.ECMWFdload(['20100601','20100901'], hr='14', filedir='./data/ERA5', model='ERA5', snwe=(30,40,120,140))
+
+print('------------------------------------------------')
+print('Downloads OK')
+print('------------------------------------------------')
