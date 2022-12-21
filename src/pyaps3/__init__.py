8118566e266a55cbe30087e6403caf569ae85f96
Zhang Yunjun <yunjunzgeo@gmail.com>
2022-12-20 23:14:17 -0800

    packaging: `pyproject.toml` + `setuptools_scm` (#19)
    + replace `setup.py` with `pyproject.toml` file
    
    + replace `version.py` with `setuptools_scm` within `pyproject.toml` file
    
    + requirements.txt: add packaging-related dependencies
    
    + circleci: 
       - install git to fix installation error
       - use `BASH_ENV` to share PATH among run steps
       - rm `fortran-compiler` as it's not used
    
    + dependency: set python>=3.8
    
    + `__init__`: fix `__all__`

--- src/pyaps3/__init__.py
+++ src/pyaps3/__init__.py
@@ -1,19 +1,29 @@
 '''
 PyAPS module to compute InSAR phase delay maps from weather models.
 
-Written by Romain Jolivet <rjolivet@gps.caltech.edu> and Piyush Agram <piyush@gps.caltech.edu>. The original Fortran package was written by Romain Jolivet and the Python version including support for different models was written by Piyush Agram.
+Written by Romain Jolivet <rjolivet@gps.caltech.edu> and Piyush Agram <piyush@gps.caltech.edu>.
+The original Fortran package was written by Romain Jolivet and the Python version including
+support for different models was written by Piyush Agram.
 
 .. note::
-    Details of the python module can be obtained `here. <http://code.google.com/p/pyaps>`_
+    Details of the python module can be obtained `here. <http://code.google.com/p/pyaps>`
 '''
 
-__all__ = ['autoget','objects']
+# get version info [requires python >=3.8]
+from importlib.metadata import PackageNotFoundError, version
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:
+    print('package is not installed!\n'
+          'Please follow the installation instructions in the README.md.\n'
+          'Or, to just get the version number, use:\n'
+          '   python -m setuptools_scm')
 
+# top-level functions
 from pyaps3.objects import PyAPS
 from pyaps3.autoget import ECMWFdload, MERRAdload, NARRdload
 
-# get version info
-from pyaps3.version import release_version as __version__
+__all__ = ['__version__', 'PyAPS', 'ECMWFdload', 'MERRAdload', 'NARRdload']
 
 ############################################################
 # Program is part of PyAPS                                 #
